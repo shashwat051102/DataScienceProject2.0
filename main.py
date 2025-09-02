7f950b20908a6bc96dda1cbe0d41a54f38ef8533
Shashwat singh <shashwatsingh051102@gmail.com>
2025-09-03 00:17:11 +0530

    Model Training

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.DataScienceProject import logger
 from src.DataScienceProject.pipeline.Data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.DataScienceProject.pipeline.Data_validation_pipeline import DataValidationTrainingPipeline
 from src.DataScienceProject.pipeline.Data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.DataScienceProject.pipeline.Model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -9,8 +10,8 @@ STAGE_NAME = "Data Ingestion Stage"
 if __name__ == "__main__":
     try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.initiate_data_ingestion()
+        obj_ingestion = DataIngestionTrainingPipeline()
+        obj_ingestion.initiate_data_ingestion()
         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
@@ -20,8 +21,8 @@ if __name__ == "__main__":
     STAGE_NAME = "Data Validation Stage"
     try:
         logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
-        obj = DataValidationTrainingPipeline()
-        obj.initiate_data_validation()
+        obj_validation = DataValidationTrainingPipeline()
+        obj_validation.initiate_data_validation()
         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx==========x")
         
     except Exception as e:
@@ -33,10 +34,22 @@ if __name__ == "__main__":
     
     try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-        obj = DataTransformationTrainingPipeline()
-        obj.initiate_data_transformation()
+        obj_transformation = DataTransformationTrainingPipeline()
+        obj_transformation.initiate_data_transformation()
         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
         
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+    STAGE_NAME = "Model Trainer Stage"
+    
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj_training = ModelTrainerTrainingPipeline()
+        obj_training.initiate_model_training()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
---
+++ src/DataScienceProject/pipeline/Model_trainer_pipeline.py
@@ -0,0 +1,17 @@
+from src.DataScienceProject.config.configuration import ConfigurationManager
+from src.DataScienceProject.components.Model_traienr import ModelTrainer
+from src.DataScienceProject import logger
+
+
+
+STAGE_NAME = "Model Trainer Stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def initiate_model_training(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer = ModelTrainer(config=model_trainer_config)
+        model_trainer.train()
